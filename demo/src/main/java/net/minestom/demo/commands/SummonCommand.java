547fa250cfcf2cf1358ee5913a4c93fdf7425d19
kermandev <46640204+kermandev@users.noreply.github.com>
2025-05-20 18:39:54 -0500

    Undo testing version of summon command
    its bad code lol

--- demo/src/main/java/net/minestom/demo/commands/SummonCommand.java
+++ demo/src/main/java/net/minestom/demo/commands/SummonCommand.java
@@ -7,22 +7,17 @@ import net.minestom.server.command.builder.arguments.Argument;
 import net.minestom.server.command.builder.arguments.ArgumentEnum;
 import net.minestom.server.command.builder.arguments.ArgumentType;
 import net.minestom.server.command.builder.arguments.minecraft.registry.ArgumentEntityType;
-import net.minestom.server.command.builder.arguments.number.ArgumentInteger;
 import net.minestom.server.command.builder.condition.Conditions;
 import net.minestom.server.coordinate.Vec;
 import net.minestom.server.entity.*;
 import net.minestom.server.utils.location.RelativeVec;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Random;
-
 public class SummonCommand extends Command {
 
     private final ArgumentEntityType entity;
     private final Argument<RelativeVec> pos;
     private final Argument<EntityClass> entityClass;
-    private final ArgumentInteger amount;
-    private final ArgumentInteger radius;
 
     public SummonCommand() {
         super("summon");
@@ -37,40 +32,14 @@ public class SummonCommand extends Command {
         entityClass = ArgumentType.Enum("class", EntityClass.class)
                 .setFormat(ArgumentEnum.Format.LOWER_CASED)
                 .setDefaultValue(EntityClass.CREATURE);
-
-        amount = ArgumentType.Integer("amount");
-        amount.setDefaultValue(1);
-
-        radius = ArgumentType.Integer("radius");
-        radius.setDefaultValue(0);
-
-        addSyntax(this::execute, entity, pos, entityClass, amount, radius);
-        setDefaultExecutor((sender, context) -> sender.sendMessage("Usage: /summon <type> <x> <y> <z> <class> [amount] [radius]"));
+        addSyntax(this::execute, entity, pos, entityClass);
+        setDefaultExecutor((sender, context) -> sender.sendMessage("Usage: /summon <type> <x> <y> <z> <class>"));
     }
 
     private void execute(@NotNull CommandSender commandSender, @NotNull CommandContext commandContext) {
-        final EntityClass entityClass = commandContext.get(this.entityClass);
-        final EntityType entityType = commandContext.get(this.entity);
-        final Vec pos = commandContext.get(this.pos).fromSender(commandSender);
-        final int amount = commandContext.get(this.amount);
-        final int radius = commandContext.get(this.radius);
-
-        final Random random = new Random();
-
-        var startTime = System.nanoTime();
-        int spawnedGood = 0;
-        for (int i = 0; i < amount; i++) {
-            final Entity entity = entityClass.instantiate(entityType);
-            //noinspection ConstantConditions - One couldn't possibly execute a command without being in an instance
-            try {
-                entity.setInstance(((Player) commandSender).getInstance(), pos.add(random.nextDouble() * 2 * radius - radius, 0, random.nextDouble() * 2 * radius - radius));
-                spawnedGood++;
-            } catch (Exception e) {
-                System.out.println("failed spawn for " + i);
-            }
-        }
-        System.out.println("Took " + (System.nanoTime() - startTime) / 1e6 + "ms");
-        System.out.println("Done! with " + spawnedGood);
+        final Entity entity = commandContext.get(entityClass).instantiate(commandContext.get(this.entity));
+        //noinspection ConstantConditions - One couldn't possibly execute a command without being in an instance
+        entity.setInstance(((Player) commandSender).getInstance(), commandContext.get(pos).fromSender(commandSender));
     }
 
     @SuppressWarnings("unused")
@@ -78,7 +47,6 @@ public class SummonCommand extends Command {
         BASE(Entity::new),
         LIVING(LivingEntity::new),
         CREATURE(EntityCreature::new);
-        //ZOMBIE(ZombieCreature::new);
         private final EntityFactory factory;
 
         EntityClass(EntityFactory factory) {
@@ -90,7 +58,6 @@ public class SummonCommand extends Command {
         }
     }
 
-    @FunctionalInterface
     interface EntityFactory {
         Entity newInstance(EntityType type);
     }
