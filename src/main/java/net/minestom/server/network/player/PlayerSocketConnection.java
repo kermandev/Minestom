1577121f687f5c6fb7a2e30276437a4cacf74650
kermandev <46640204+kermandev@users.noreply.github.com>
2025-10-31 12:44:05 -0500

    Revert "Force encryption to be set on the read thread"
    This reverts commit f0f406a3a1c1a1cece5946c71d872b7138df1dab.

--- src/main/java/net/minestom/server/network/player/PlayerSocketConnection.java
+++ src/main/java/net/minestom/server/network/player/PlayerSocketConnection.java
@@ -74,7 +74,7 @@ public class PlayerSocketConnection extends PlayerConnection {
     private SocketAddress remoteAddress;
 
     //Could be null. Only used for Mojang Auth
-    private @Nullable EncryptionContext encryptionContext;
+    private volatile @Nullable EncryptionContext encryptionContext;
     private byte[] nonce = new byte[4];
 
     // Data from client packets
@@ -184,7 +184,6 @@ public class PlayerSocketConnection extends PlayerConnection {
      */
     public void setEncryptionKey(SecretKey secretKey) {
         Check.stateCondition(encryptionContext != null, "Encryption is already enabled!");
-        Check.stateCondition(Thread.currentThread() != readThread, "Read thread must set the encryption key.");
         this.encryptionContext = new EncryptionContext(MojangCrypt.getCipher(1, secretKey), MojangCrypt.getCipher(2, secretKey));
     }
 
