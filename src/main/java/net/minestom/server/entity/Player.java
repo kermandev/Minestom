57ff58a3b1cde353fc379aedde87febb2daba6a0
kermandev <46640204+kermandev@users.noreply.github.com>
2026-02-08 14:32:12 -0600

    fix: GraalVM native image
    Viewable still is a problem, but can be alleviated if you turn the pooling for Network buffers for viewable to zero.
    
    Tested with:
    native-image --initialize-at-build-time -march=native --enable-https -jar minestom-demo.jar -Dminestom.viewable-packet=false

--- src/main/java/net/minestom/server/entity/Player.java
+++ src/main/java/net/minestom/server/entity/Player.java
@@ -117,8 +117,6 @@ import java.util.function.UnaryOperator;
  * You can easily create your own implementation of this and use it with {@link ConnectionManager#setPlayerProvider(PlayerProvider)}.
  */
 public class Player extends LivingEntity implements CommandSender, HoverEventSource<ShowEntity>, NamedAndIdentified {
-    private static final DynamicRegistry<DimensionType> DIMENSION_TYPE_REGISTRY = MinecraftServer.getDimensionTypeRegistry();
-
     private static final Component REMOVE_MESSAGE = Component.text("You have been removed from the server without reason.", NamedTextColor.RED);
     private static final Component MISSING_REQUIRED_RESOURCE_PACK = Component.text("Required resource pack was not loaded.", NamedTextColor.RED);
 
@@ -251,7 +249,7 @@ public class Player extends LivingEntity implements CommandSender, HoverEventSou
         refreshAnswerKeepAlive(true);
 
         this.gameMode = GameMode.SURVIVAL;
-        this.dimensionTypeId = DIMENSION_TYPE_REGISTRY.getId(DimensionType.OVERWORLD); // Default dimension
+        this.dimensionTypeId = MinecraftServer.process().dimensionType().getId(DimensionType.OVERWORLD); // Default dimension
         this.levelFlat = true;
 
         // FakePlayer init its connection there
@@ -282,7 +280,7 @@ public class Player extends LivingEntity implements CommandSender, HoverEventSou
         this.pendingInstance = null;
 
         this.removed = false;
-        this.dimensionTypeId = DIMENSION_TYPE_REGISTRY.getId(spawnInstance.getDimensionType());
+        this.dimensionTypeId = MinecraftServer.process().dimensionType().getId(spawnInstance.getDimensionType());
 
         final JoinGamePacket joinGamePacket = new JoinGamePacket(
                 getEntityId(), this.hardcore, List.of(), 0,
@@ -1607,7 +1605,7 @@ public class Player extends LivingEntity implements CommandSender, HoverEventSou
      * @return the player current dimension
      */
     public DimensionType getDimensionType() {
-        return DIMENSION_TYPE_REGISTRY.get(dimensionTypeId);
+        return Objects.requireNonNull(MinecraftServer.process().dimensionType().get(dimensionTypeId));
     }
 
     public PlayerInventory getInventory() {
@@ -1687,7 +1685,7 @@ public class Player extends LivingEntity implements CommandSender, HoverEventSou
     protected void sendDimension(RegistryKey<DimensionType> dimensionType, String dimensionName) {
         Check.argCondition(instance.getDimensionName().equals(dimensionName),
                 "The dimension needs to be different than the current one!");
-        this.dimensionTypeId = DIMENSION_TYPE_REGISTRY.getId(dimensionType);
+        this.dimensionTypeId = MinecraftServer.process().dimensionType().getId(dimensionType);
         sendPacket(new RespawnPacket(dimensionTypeId, dimensionName,
                 0, gameMode, gameMode, false, levelFlat,
                 deathLocation, portalCooldown, DEFAULT_SEA_LEVEL, (byte) RespawnPacket.COPY_ALL));
