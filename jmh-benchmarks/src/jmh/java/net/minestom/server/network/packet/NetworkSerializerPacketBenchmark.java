d423bf337a82085fd6a742bdd4eae37cbb51b22e
Kerman <46640204+kermandev@users.noreply.github.com>
2026-02-10 15:26:25 -0600

    refactor: Change benchmark to use a bigger packet

--- jmh-benchmarks/src/jmh/java/net/minestom/server/network/packet/NetworkSerializerPacketBenchmark.java
+++ jmh-benchmarks/src/jmh/java/net/minestom/server/network/packet/NetworkSerializerPacketBenchmark.java
@@ -1,13 +1,12 @@
 package net.minestom.server.network.packet;
 
-import net.minestom.server.MinecraftServer;
-import net.minestom.server.network.ConnectionState;
+import net.minestom.server.entity.GameMode;
 import net.minestom.server.network.NetworkBuffer;
-import net.minestom.server.network.packet.server.ServerPacket;
-import net.minestom.server.network.packet.server.common.KeepAlivePacket;
+import net.minestom.server.network.packet.server.play.JoinGamePacket;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Blackhole;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
@@ -17,28 +16,33 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class NetworkSerializerPacketBenchmark {
+    private static final JoinGamePacket PACKET = new JoinGamePacket(
+            12345, true, List.of("world", "nether", "end"), 100, 10, 10,
+            false, true, false, 0, "world", 123456789L, GameMode.SURVIVAL,
+            GameMode.CREATIVE, false, false, null, 0, 64, true
+    );
 
-    private KeepAlivePacket packet;
+    private JoinGamePacket packet;
     private NetworkBuffer readBuffer;
     private NetworkBuffer writeBuffer;
 
     @Setup(Level.Iteration)
     public void setup() {
-        packet = new KeepAlivePacket(0);
+        packet = PACKET;
         readBuffer = NetworkBuffer.staticBuffer(256);
-        readBuffer.write(KeepAlivePacket.SERIALIZER, new KeepAlivePacket(12451235));
+        readBuffer.write(JoinGamePacket.SERIALIZER, PACKET);
         writeBuffer = NetworkBuffer.staticBuffer(256);
     }
 
     @Benchmark
     public void writePacket(Blackhole blackhole) {
-        writeBuffer.writeAt(0, KeepAlivePacket.SERIALIZER, packet);
+        writeBuffer.writeAt(0, JoinGamePacket.SERIALIZER, packet);
         blackhole.consume(writeBuffer);
     }
 
     @Benchmark
     public void readPacket(Blackhole blackhole) {
-        blackhole.consume(readBuffer.readAt(0, KeepAlivePacket.SERIALIZER));
+        blackhole.consume(readBuffer.readAt(0, JoinGamePacket.SERIALIZER));
     }
 
     @TearDown
