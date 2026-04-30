d9aa99c4361c6c9a05887b00b26feb3a0fc88b6a
kermandev <46640204+kermandev@users.noreply.github.com>
2026-04-29 21:38:51 -0500

    wip

--- src/main/java/net/minestom/server/network/NetworkBuffer.java
+++ src/main/java/net/minestom/server/network/NetworkBuffer.java
@@ -85,6 +85,9 @@ public sealed interface NetworkBuffer permits NetworkBufferImpl {
 
     static <E extends Enum<E>> Type<E> Enum(Class<E> enumClass) {
         final E[] values = enumClass.getEnumConstants();
+        if (values.length < 128) {
+            return BYTE.transform(integer -> values[integer], (it) -> (byte) it.ordinal());
+        }
         return VAR_INT.transform(integer -> values[integer], Enum::ordinal);
     }
 
